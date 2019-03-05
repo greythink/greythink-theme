b33e97c9d1f0bf065fa8e44d0f788675129596ca
Archit Chandra <architchandra@gmail.com>
2019-03-05 19:40:44 +0530

    Add content to the front page

--- front-page.php
+++ front-page.php
@@ -20,64 +20,65 @@
   </div><!-- row-intro-text -->
 
   <div class="row row-service-feature align-items-center service-feature-left">
-    <div class="col-md-7">
+    <div class="col-md-6">
       <div class="service-feature-text">
         <h3>High-quality technology</h3>
         <ul>
-          <li><span>Painstaking attention to detail</span></li>
-          <li><span>Focus on usability and convenience</span></li>
-          <li><span>Target school-specific positioning using proprietary frameworks</span></li>
+          <li><span>Use of proven, future-proof techologies and tools</span></li>
+          <li><span>Thorough testing before deployment</span></li>
+          <li><span>Search engine optimized code</span></li>
+          <li><span>Equally good performance on mobiles and tablets</span></li>
         </ul>
       </div>
     </div>
-    <div class="col-md-5 service-feature-image">
-      <img src="<?php echo wp_get_attachment_image_src(106, 'medium_large')[0]; ?>" alt="">
+    <div class="col-md-6 service-feature-image">
+      <img src="<?php echo wp_get_attachment_image_src(116, 'medium_large')[0]; ?>" alt="">
     </div>
   </div>
 
   <div class="row row-service-feature align-items-center service-feature-right">
-    <div class="col-md-5 service-feature-image">
-      <img src="<?php echo wp_get_attachment_image_src(105, 'medium_large')[0]; ?>" alt="">
+    <div class="col-md-6 service-feature-image">
+      <img src="<?php echo wp_get_attachment_image_src(115, 'medium_large')[0]; ?>" alt="">
     </div>
-    <div class="col-md-7">
+    <div class="col-md-6">
       <div class="service-feature-text">
         <h3>Intuitive Design</h3>
         <ul>
-          <li><span>Customized strategies and tactics</span></li>
-          <li><span>Constant feedback, answers and guidance via email and phone</span></li>
-          <li><span>Optional teleconference on post-greythink career planning</span></li>
-          <li><span>Optional translation services for recommendations</span></li>
+          <li><span>Painstaking attention to detail</span></li>
+          <li><span>Focus on user-experience, usability and convenience</span></li>
+          <li><span>Clean and visually appealing design</span></li>
+          <li><span>Mobile-friendly layouts</span></li>
         </ul>
       </div>
     </div>
   </div>
 
   <div class="row row-service-feature align-items-center service-feature-left">
-    <div class="col-md-7">
+    <div class="col-md-6">
       <div class="service-feature-text">
         <h3>Impeccable Service</h3>
         <ul>
-          <li><span>Multiple iterations of resume review and refinement</span></li>
-          <li><span>Thorough critiques and comments on recommendation outlines</span></li>
-          <li><span>Painstakingly detailed reviews of essay outlines</span></li>
-          <li><span>Suggestions for stylistic improvements of narrative drafts</span></li>
+          <li><span>Highly-skilled and reliable team</span></li>
+          <li><span>Constant communication to set the right expectations</span></li>
+          <li><span>Skin in the game-level dedication</span></li>
+          <li><span>Just a phone call away, especially in times of need</span></li>
         </ul>
       </div>
     </div>
-    <div class="col-md-5 service-feature-image">
-      <img src="<?php echo wp_get_attachment_image_src(107, 'medium_large')[0]; ?>" alt="">
+    <div class="col-md-6 service-feature-image">
+      <img src="<?php echo wp_get_attachment_image_src(114, 'medium_large')[0]; ?>" alt="">
     </div>
   </div>
 
   <div class="row row-internal-cta">
     <div class="col-md-7 d-flex flex-column justify-content-center internal-cta-column">
-      <h3>Get your profile evaluated</h3>
+      <h3>Turn your idea into a great product</h3>
       <p class="internal-cta-text">
-        Our team will get back to you with the results in a few days
+        Give us a few details about what you want to build and let's get started.
       </p>
     </div>
     <div class="col-md-5 d-flex flex-column justify-content-center internal-cta-column align-items-center">
-      <a href="#" class="btn btn-lg btn-block">Request a free evaluation</a>
+      <a href="#" class="btn btn-lg btn-block">Write to us</a>
     </div>
   </div>
 
